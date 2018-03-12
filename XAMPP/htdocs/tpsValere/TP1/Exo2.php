e7bed1236457624ec164620509043ef9f1d466b5
AdminEtu <Vapi27v@gmail.com>
2018-03-12 16:56:36 +0100

    Crypto Mining to the Moon

--- XAMPP/htdocs/tpsValere/TP1/Exo2.php
+++ XAMPP/htdocs/tpsValere/TP1/Exo2.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Hello</title>
+<title>CryptoMining For ever</title>
 </head>
 <body>
+    <a href="?couleur=red&size=15&message=SAlut">Rouge et petit</a><br>
+    <a href="?couleur=green&size=30&message=SAlut">Vert Moyen</a><br>
+    <a href="?couleur=blue&size=50&message=SAlut">Bleu et Grand</a><br>
     <form method="GET">
     <label for="size">Size : </label>
     <input type="number" value="" name="size" id="size"><br>
@@ -16,7 +19,10 @@
 </form>
    <?php
     echo "<div>Message de taille {{$_GET["size"]}}px en {$_GET["couleur"]}</div>";
-    echo "<p style='color: {$_GET["couleur"]};'>{$_GET["message"]}</p>";
+    echo "<p style='color: {$_GET["couleur"]};size: {$_GET["size"]}px;'>{$_GET["message"]}</p>";
+    if (empty($_GET["couleur"])){
+      echo "<p style='color:red'>Vous n'avez pas remplis tout les champs</p>"; 
+    }
     ?>
 </body>
 </html>
